0db6f27433b682678018053a131b1082ec993658
Ting Chien Meng <tcm390@nyu.edu>
2025-05-17 10:00:43 +0800

    switch to forked bootstrap plugin

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import {
   type ProjectAgent,
 } from '@elizaos/core';
 import hyperfyPlugin from './plugin-hyperfy';
-import bootstrapPlugin from '@elizaos/plugin-bootstrap';
+import bootstrapPlugin from './plugin-bootstrap';
 
 /**
  * Represents the default character (Eliza) with her specific attributes and behaviors.
